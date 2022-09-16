61f3a7ed74bd965ce9d087712504025ebab65926
Achinit99 <achinit720@gmail.com>
2022-09-16 14:13:24 +0530

    Assigment Operators

--- script.js
+++ script.js
@@ -8,13 +8,26 @@ console.log(firstName);
 year = 1997;
 console.log(typeof year);
 
-// Basic Operators
+// Math Operators
 const now = 2022;
 const ageachini = now - 1999;
-const ageavishka = now - 1998;
+const ageavishka = now - 1995;
 console.log(ageachini, ageavishka);
 console.log(ageachini + ageavishka);
 
 const First_N = "Ahinsa";
 const Last_N = "Thathsarani";
-console.log(First_N + " " + Last_N); 
+console.log(First_N + " " + Last_N);
+
+// Assigment Operators
+let a = 10 + 5; // 15
+a += 10; // a = a + 10 = 25
+a *= 4; // a = a * 4 = 100
+a++; // a = a + 1
+a--; // a = a - 1
+a--;
+console.log(a);
+
+// Comparison Operators
+console.log(ageachini < ageavishka);  // >, <, >=, <=
+console.log(ageavishka >= 18);
