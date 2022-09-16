29c1ee72efc0526b36b6c6ef69169a7336094615
Achinit99 <achinit720@gmail.com>
2022-09-16 13:33:27 +0530

    Basic_Operators

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 let js = "amazing";
-if (js === "amazing") alert("Javascript is Good! and Fun!");
-
 console.log(40 + 8 + 23 - 10);
+
+// Variable name conventions
+let firstName = "Achini";
+console.log(firstName);
+
+year = 1997;
+console.log(typeof year);
+
+// Basic Operators
+const now = 2022;
+const ageachini = now - 1999;
+const ageavishka = now - 1998;
+console.log(ageachini, ageavishka);
+console.log(ageachini + ageavishka);
+
+const First_N = "Ahinsa";
+const Last_N = "Thathsarani";
+console.log(First_N + " " + Last_N); 
