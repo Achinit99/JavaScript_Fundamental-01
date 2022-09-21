f63d37dc66b11c36c2317e0f5c69c9d9ee3cbb33
Achinit99 <achinit720@gmail.com>
2022-09-21 12:30:05 +0530

    The switch Statment -> if else block,

--- script.js
+++ script.js
@@ -252,3 +252,19 @@ switch (day) {
     default:
         console.log ("Not a valid day!");
 }
+// The switch Statment -> if else block,
+if (day === "monday") {
+    console.log ("Plan course structure");
+    console.log ("Go to coding meetup");
+} else if (day === "tuesday") {
+    console.log ("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+    console.log ("Write code examples");
+} else if (day === "friday") {
+    console.log ("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+    console.log ("Enjoy the weekend :D");
+} else {
+    console.log ("Not a valid day!");
+}
+
