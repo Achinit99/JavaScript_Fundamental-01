db6e11a37f4badf09746784000cf1fad5c2fb79a
Achinit99 <achinit720@gmail.com>
2022-09-21 12:11:53 +0530

    The switch Statment

--- script.js
+++ script.js
@@ -209,7 +209,7 @@ if (hasDriversLicense && hasGoodVision && ! newisTired){
 } else {
     console.log ("Someone else should drive..");
 }
-*/
+
 
 // Coding challenge --> #3
 const scoreDolphins = (97 + 112 + 80) / 3;
@@ -224,4 +224,31 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
     console.log ("Both win the trophy!");
 } else {
     console.log ("No one wins the trophy 😭");
+}
+*/
+
+// The switch Statment
+const day = "monday";
+
+switch (day) {
+    case "monday": // day === "monday"
+        console.log ("Plan course structure");
+        console.log ("Go to coding meetup");
+        break;
+    case "tuesday":
+        console.log ("Prepare theory videos");
+        break;
+    case "wednesday":
+    case "thursday":
+        console.log ("Write code examples");
+        break;
+    case "friday":
+        console.log ("Record videos");
+        break;
+    case "saturday":
+    case "sunday":
+        console.log ("Enjoy the weekend :D");
+        break;
+    default:
+        console.log ("Not a valid day!");
 }
