41a8313c56f1bfdfb844d647bdd0b1fb95ee3fd2
Achinit99 <achinit720@gmail.com>
2022-10-17 11:34:27 +0530

    Life in Weeks Solutions

--- script.js
+++ script.js
@@ -304,3 +304,31 @@ const bill = 430;
 const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 console.log (`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
 */
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+// Life in Weeks Solutions
+
+/* it will take your current age as the inout and console.logs a message with our time left in this format:
+   You have x days, y weeks, and z months left.
+   Where x,y and z are replaced with the actual calculated numbers.
+ */
+
+function lifeInWeeks(age) {
+  var yearsRemaining = 90 - age;
+  var days = yearsRemaining * 365;
+  var weeks = yearsRemaining * 52;
+  var months = yearsRemaining * 12;
+
+  console.log(
+    "You have " +
+      days +
+      " days , " +
+      weeks +
+      " weeks, and " +
+      months +
+      " months left. "
+  );
+}
+
+lifeInWeeks(10);
