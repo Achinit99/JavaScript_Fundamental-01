99a18b7fad3b752a5788ca5789d40663ce00d259
Achinit99 <achinit720@gmail.com>
2023-01-12 10:53:14 +0530

    prompt input and alert in output

--- Anjala-lesson/variables.js
+++ Anjala-lesson/variables.js
@@ -3,3 +3,8 @@ alert(myName); // display the name
 
 /* how to take a promt in user input */
 var yourName = prompt("What is your name?");
+
+// prompt input and alert in output
+var myName = "Achini";
+var yourName = prompt("What is your name ?");
+alert("My name is " + myName + ", welcome to my course " + yourName + " !");
